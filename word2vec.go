610a6a5f337b4ae0aca5647519ad9513daf03f58
David Howden <dhowden@gmail.com>
2015-08-05 12:05:04 +1000

    Added word2vec initial implementation

---
+++ word2vec.go
@@ -0,0 +1,165 @@
+// Package word2vec provides functionality for reading binary word2vec models
+// and basic usage (see https://code.google.com/p/word2vec/).
+package word2vec
+
+import (
+	"bufio"
+	"encoding/binary"
+	"fmt"
+	"io"
+
+	"github.com/ziutek/blas"
+)
+
+// Model is a type which represents a word2vec Model.
+type Model struct {
+	dim   int
+	words map[string]Vector
+}
+
+// FromReader creates a Model using the binary model data provided by the io.Reader.
+func FromReader(r io.Reader) (*Model, error) {
+	br := bufio.NewReader(r)
+	var size, dim int
+	n, err := fmt.Fscanln(r, &size, &dim)
+	if err != nil {
+		return nil, err
+	}
+	if n != 2 {
+		return nil, fmt.Errorf("could not extract size/dim from binary Data")
+	}
+
+	m := &Model{
+		words: make(map[string]Vector, size),
+		dim:   dim,
+	}
+
+	raw := make([]float32, size*dim)
+
+	for i := 0; i < size; i++ {
+		w, err := br.ReadString(' ')
+		if err != nil {
+			return nil, err
+		}
+		w = w[:len(w)-1]
+
+		v := Vector(raw[dim*i : m.dim*(i+1)])
+		err = binary.Read(br, binary.LittleEndian, v)
+		if err != nil {
+			return nil, err
+		}
+
+		v.Normalise()
+
+		_, err = br.ReadByte()
+		if err != nil {
+			return nil, err
+		}
+
+		m.words[w] = v
+	}
+	return m, nil
+}
+
+// Vector is a type which represents a word vector.
+type Vector []float32
+
+// Normalise normalises the vector in-place.
+func (v Vector) Normalise() {
+	w := blas.Snrm2(len(v), v, 1)
+	blas.Sscal(len(v), 1/w, v, 1)
+}
+
+// Add performs v += a * u (in-place).
+func (v Vector) Add(a float32, u Vector) {
+	blas.Saxpy(len(v), a, u, 1, v, 1)
+}
+
+// Dot computes the dot product with u.
+func (v Vector) Dot(u Vector) float32 {
+	return blas.Sdot(len(v), u, 1, v, 1)
+}
+
+// NotFoundError is an error returned from Model functions when an input
+// word is not in the model.
+type NotFoundError struct {
+	Word string
+}
+
+func (e NotFoundError) Error() string {
+	return fmt.Sprintf("word not found: %v", e.Word)
+}
+
+// Size returns the number of words in the model.
+func (m *Model) Size() int {
+	return len(m.words)
+}
+
+// Dim returns the dimention of the vectors in the model.
+func (m *Model) Dim() int {
+	return m.dim
+}
+
+// Similarity returns the similarity between the two words.
+func (m *Model) Similarity(x, y string) (float32, error) {
+	u, ok := m.words[x]
+	if !ok {
+		return 0.0, &NotFoundError{x}
+	}
+	v, ok := m.words[y]
+	if !ok {
+		return 0.0, &NotFoundError{y}
+	}
+	return u.Dot(v), nil
+}
+
+// Eval constructs a vector by adding and subtracting the vector values of
+// lists of words.
+func (m *Model) Eval(add []string, sub []string) (Vector, error) {
+	v := Vector(make([]float32, m.dim))
+	for _, w := range add {
+		u, ok := m.words[w]
+		if !ok {
+			return nil, &NotFoundError{w}
+		}
+		v.Add(1, u)
+	}
+	for _, w := range sub {
+		u, ok := m.words[w]
+		if !ok {
+			return nil, &NotFoundError{w}
+		}
+		v.Add(-1, u)
+	}
+	v.Normalise()
+	return v, nil
+}
+
+// Pair is a type which represents a pairing of a word and score indicating
+// the similarity of this word against a search word.
+type Pair struct {
+	Word  string
+	Score float32
+}
+
+// MostSimilar is a method which returns a list of `n` most similar vectors
+// to `v` in the model.
+func (m *Model) MostSimilar(v Vector, n int) ([]Pair, error) {
+	r := make([]Pair, n)
+	for w, u := range m.words {
+		score := v.Dot(u)
+		p := Pair{w, score}
+		// TODO(dhowden): MaxHeap would be better here if n is large.
+		if r[n-1].Score > p.Score {
+			continue
+		}
+		r[n-1] = p
+		for j := n - 2; j >= 0; j-- {
+			if r[j].Score > p.Score {
+				break
+			}
+			r[j], r[j+1] = p, r[j]
+		}
+	}
+	return r, nil
+}
