7495efa55e355d0f10a8acb24bd2aca8f8767a54
bartfokker <bart@publicsonar.com>
2018-07-24 20:31:42 +0200

    added backwards compatability

--- word2vec_backward.go
+++ word2vec_backward.go
@@ -1,4 +1,4 @@
-// +build go1.8
+// +build !go1.8
 
 // Package word2vec provides functionality for reading binary word2vec models
 // and performing cosine similarity queries (see https://code.google.com/p/word2vec/).
@@ -11,7 +11,7 @@ import (
 	"io"
 	"sync"
 
-	"gonum.org/v1/gonum/blas/blas32"
+	"github.com/ziutek/blas"
 )
 
 // Model is a type which represents a word2vec Model and implements the Coser
@@ -82,31 +82,23 @@ type Vector []float32
 
 // Normalise normalises the vector in-place.
 func (v Vector) Normalise() {
-	w := blas32.Nrm2(len(v), blas32.Vector{Inc: 1, Data: v})
-	blas32.Scal(len(v), 1/w, blas32.Vector{Inc: 1, Data: v})
+	w := blas.Snrm2(len(v), v, 1)
+	blas.Sscal(len(v), 1/w, v, 1)
 }
 
 // Norm computes the Euclidean norm of the vector.
 func (v Vector) Norm() float32 {
-	return blas32.Nrm2(len(v), blas32.Vector{Inc: 1, Data: v})
+	return blas.Snrm2(len(v), v, 1)
 }
 
 // Add performs v += a * u (in-place).
 func (v Vector) Add(a float32, u Vector) {
-	blas32.Axpy(len(v), a, blas32.Vector{Inc: 1, Data: u}, blas32.Vector{Inc: 1, Data: v})
+	blas.Saxpy(len(v), a, u, 1, v, 1)
 }
 
 // Dot computes the dot product with u.
 func (v Vector) Dot(u Vector) float32 {
-	x := blas32.Vector{
-		Inc:  1,
-		Data: u,
-	}
-	y := blas32.Vector{
-		Inc:  1,
-		Data: v,
-	}
-	return blas32.Dot(len(v), x, y)
+	return blas.Sdot(len(v), u, 1, v, 1)
 }
 
 // NotFoundError is an error returned from Model functions when an input
