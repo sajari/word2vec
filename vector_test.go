8ea88b1e74d814cd874c6f18e4ee207d6cc07c86
bartfokker <bart@publicsonar.com>
2018-08-10 16:59:54 +0200

    move vector + tests to seperate file

--- vector_test.go
+++ vector_test.go
@@ -1,6 +1,7 @@
 package word2vec
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
 )
@@ -64,5 +65,58 @@ func TestVectorDot(t *testing.T) {
 		if ans != tt.ans {
 			t.Errorf("x.Dot(y) = %v, expected %v", ans, tt.ans)
 		}
+	}
+}
+
+func BenchmarkDotFloat32(b *testing.B) {
+	for _, bm := range []int{10, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500} {
+		b.Run(fmt.Sprintf("test with dimension %d", bm), func(b *testing.B) {
+			vData := data(bm)
+			uData := data(bm)
+			for i := 0; i < b.N; i++ {
+				v := Vector(vData)
+				u := Vector(uData)
+				v.Dot(u)
+			}
+		})
+	}
+}
+
+func BenchmarkAddFloat32(b *testing.B) {
+	benchmarks := []struct {
+		dim int
+		a   float32
+	}{}
+	for i, dim := range []int{10, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500} {
+		benchmarks = append(benchmarks, struct {
+			dim int
+			a   float32
+		}{
+			dim: dim,
+			a:   float32(i * 10),
+		})
+	}
+	for _, bm := range benchmarks {
+		b.Run(fmt.Sprintf("test with dimension %d and a %.2f", bm.dim, bm.a), func(b *testing.B) {
+			vData := data(bm.dim)
+			uData := data(bm.dim)
+			for i := 0; i < b.N; i++ {
+				v := Vector(vData)
+				u := Vector(uData)
+				v.Add(bm.a, u)
+			}
+		})
+	}
+}
+
+func BenchmarkNormFloat32(b *testing.B) {
+	for _, bm := range []int{10, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500} {
+		b.Run(fmt.Sprintf("test with dimension %d", bm), func(b *testing.B) {
+			d := data(bm)
+			for i := 0; i < b.N; i++ {
+				v := Vector(d)
+				v.Norm()
+			}
+		})
 	}
 }
