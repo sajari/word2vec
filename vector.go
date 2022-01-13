3b694b0ef17de4b9261d75e5184d2c52ea698540
David Howden <dhowden@gmail.com>
2022-01-13 12:51:59 +1100

    use native Go code for vector computations on non-amd64 machines (#13)
    Corrects an issue where the BLAS library doesn't have customised code for ARM

---
+++ vector.go
@@ -0,0 +1,39 @@
+//go:build !amd64
+// +build !amd64
+
+package word2vec
+
+import "math"
+
+// Normalise normalises the vector in-place.
+func (v Vector) Normalise() {
+	n := v.Norm()
+	for i := range v {
+		v[i] /= n
+	}
+}
+
+// Norm computes the Euclidean norm of the vector.
+func (v Vector) Norm() float32 {
+	var out float32
+	for _, vx := range v {
+		out += vx * vx
+	}
+	return float32(math.Sqrt(float64(out)))
+}
+
+// Add performs v += a * u (in-place).
+func (v Vector) Add(a float32, u Vector) {
+	for i := range v {
+		v[i] += a * u[i]
+	}
+}
+
+// Dot computes the dot product with u.
+func (v Vector) Dot(u Vector) float32 {
+	var out float32
+	for i, vx := range v {
+		out += vx * u[i]
+	}
+	return out
+}
--- word2vec.go
+++ word2vec.go
@@ -8,8 +8,6 @@ import (
 	"fmt"
 	"io"
 	"sync"
-
-	"github.com/ziutek/blas"
 )
 
 // Model is a type which represents a word2vec Model and implements the Coser
@@ -77,27 +75,6 @@ func FromReader(r io.Reader) (*Model, error) {
 
 // Vector is a type which represents a word vector.
 type Vector []float32
-
-// Normalise normalises the vector in-place.
-func (v Vector) Normalise() {
-	w := blas.Snrm2(len(v), v, 1)
-	blas.Sscal(len(v), 1/w, v, 1)
-}
-
-// Norm computes the Euclidean norm of the vector.
-func (v Vector) Norm() float32 {
-	return blas.Snrm2(len(v), v, 1)
-}
-
-// Add performs v += a * u (in-place).
-func (v Vector) Add(a float32, u Vector) {
-	blas.Saxpy(len(v), a, u, 1, v, 1)
-}
-
-// Dot computes the dot product with u.
-func (v Vector) Dot(u Vector) float32 {
-	return blas.Sdot(len(v), u, 1, v, 1)
-}
 
 // NotFoundError is an error returned from Model functions when an input
 // word is not in the model.
